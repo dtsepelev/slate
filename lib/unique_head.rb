1fca9c42442d6a286d3fcebb6c6a4c3620b44503
Dmitry Tsepelev <tsepelev@yamoney.ru>
2017-09-13 21:18:37 +0300

    Fixed cyrillic headers

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,12 +1,13 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+require 'translit'
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
     @head_count = {}
   end
   def header(text, header_level)
-    friendly_text = text.parameterize
+    friendly_text = Translit.convert(text, :english).parameterize
     @head_count[friendly_text] ||= 0
     @head_count[friendly_text] += 1
     if @head_count[friendly_text] > 1
